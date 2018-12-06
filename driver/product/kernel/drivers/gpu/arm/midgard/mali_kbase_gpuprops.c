66f4a0a28cc3f3ccc21246675547cb8c0203ea5f
Neil Armstrong <narmstrong@baylibre.com>
2018-12-06 14:43:06 +0100

    Fix kbase_gpuprops for 4.20 kernel

--- driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_gpuprops.c
+++ driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_gpuprops.c
@@ -195,7 +195,11 @@ static void kbase_gpuprops_calculate_props(base_gpu_props * const gpu_props, str
 	/* Populate the base_gpu_props structure */
 	kbase_gpuprops_update_core_props_gpu_id(gpu_props);
 	gpu_props->core_props.log2_program_counter_size = KBASE_GPU_PC_SIZE_LOG2;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 20, 0)
+	gpu_props->core_props.gpu_available_memory_size = totalram_pages() << PAGE_SHIFT;
+#else
 	gpu_props->core_props.gpu_available_memory_size = totalram_pages << PAGE_SHIFT;
+#endif
 	gpu_props->core_props.num_exec_engines =
 		KBASE_UBFX32(gpu_props->raw_props.core_features, 0, 4);
 
